3bb82f5d3bf7cca289635af0a27ac0b38d6c2ebe
niilx <touhidulislamnl@gmail.com>
2020-06-19 20:28:25 +0600

    UI improved

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:food_recipe/Login/Auth.dart';
-import 'package:food_recipe/counterBloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,81 +17,6 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: LoginUser(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  CounterBloc counterbloc;
-
-  @override
-  void initState() {
-    counterbloc = CounterBloc();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    counterbloc.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text("Food Recipe"),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            StreamBuilder<int>(
-              stream: counterbloc.getStreamValue(),
-              initialData: 0,
-              builder: (context, snapshot) {
-                return Text(
-                  '${snapshot.data}',
-                  style: Theme.of(context).textTheme.headline4,
-                );
-              }
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: Row(
-        children: <Widget>[
-          FloatingActionButton(
-            onPressed: counterbloc.increment,
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-
-          FloatingActionButton(
-            onPressed: counterbloc.decrement,
-            tooltip: 'Decrement',
-            child: Icon(Icons.remove),
-          ),
-        ],
-      ),
-
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
